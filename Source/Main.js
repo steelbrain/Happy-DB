b62f0f808576f8af0bc367973c3f56467f40b5cc
steelbrain <aneesiqbalbhatti@gmail.com>
2015-04-04 09:45:39 -0700

    :new: Implement INCR

--- Source/Main.js
+++ Source/Main.js
@@ -10,7 +10,7 @@ class Main{
     this.Port = Number(Port);
     this.Server = null;
     this.Children = [];
-    this.DataBase = new Map();
+    this.Database = new Map();
   }
   Run(){
     let NumCPUs = OS.cpus().length;
@@ -27,6 +27,7 @@ class Main{
           try {
             Main['Action' + Event].call(this, Child, Request, Message);
           } catch(error){
+            console.log(error.stack);
             Message.Result = {Type: 'Error', Message: error.message};
             Child.Finished(Message);
           }
@@ -44,18 +45,46 @@ class Main{
     if(Request.length % 2 !== 0)
       throw new Error("Invalid Parameter length, Parameter number should be even");
     for(let Number = 0; Number < Request.length; Number += 2){
-      this.DataBase.set(Request[Number], Request[Number + 1]);
+      let Value = Request[Number + 1];
+      let ValueInt = parseInt(Value);
+      if(!isNaN(ValueInt) && ValueInt.toString() === Value){
+        this.Database.set(Request[Number], ValueInt);
+      } else {
+        this.Database.set(Request[Number], Value);
+      }
     }
     Message.Result = {Type: 'OK'};
     Child.Finished(Message);
   }
   static ActionGET(Child, Request, Message){
     if(Request.length === 1){
-      Message.Result = this.DataBase.get(Request[0]) || null;
+      Message.Result = this.Database.get(Request[0]) || null;
     } else {
       let ToReturn = [];
       Request.forEach(function(Name){
-        ToReturn.push(this.DataBase.get(Name) || null);
+        ToReturn.push(this.Database.get(Name) || null);
+      }.bind(this));
+      Message.Result = ToReturn;
+    }
+    Child.Finished(Message);
+  }
+  static ActionINCR(Child, Request, Message){
+    if(Request.length === 1){
+      let Value = this.Database.get(Request[0]) || 0;
+      if(typeof Value !== 'number'){
+        throw new Error("Can't INCR Non-Numeric key ${Request[0]}");
+      }
+      this.Database.set(Request[0], ++Value);
+      Message.Result = Value;
+    } else {
+      let ToReturn = [];
+      Request.forEach(function(Name){
+        let Value = this.Database.get(Name) || 0;
+        if(typeof Value !== 'number'){
+          throw new Error("Can't INCR Non-Numeric key ${Name}");
+        }
+        this.Database.set(Request[0], ++Value);
+        ToReturn.push(Value);
       }.bind(this));
       Message.Result = ToReturn;
     }
