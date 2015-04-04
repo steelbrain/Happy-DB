1be31b55a95bc14898c7a0a25b91284b5350fc8b
steelbrain <aneesiqbalbhatti@gmail.com>
2015-04-04 13:22:24 -0700

    :new: Implement RPUSH

--- Source/Methods_List.js
+++ Source/Methods_List.js
@@ -18,3 +18,19 @@ Main.ActionLPUSH = function(Request){
   Request.forEach((Item) => Value.push(Item));
   return {Type: 'OK'};
 };
+
+Main.ActionRPUSH = function(Request){
+  if(Request.length < 2){
+    throw new Error(Main.MSG_ARGS_LESS);
+  }
+  let Key = Request.shift();
+  let Value = this.Database.get(Key);
+  try {
+    Main.Validate(Main.VAL_LIST, 'LPUSH', Value);
+  } catch(err){
+    Value = [];
+    this.Database.set(Key, Value);
+  }
+  Request.reverse().forEach((Item) => Value.unshift(Item));
+  return {Type: 'OK'};
+};
