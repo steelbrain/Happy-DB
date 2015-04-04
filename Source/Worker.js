f9c39e4ff186be283ab6567bc49fa07f717a0022
steelbrain <aneesiqbalbhatti@gmail.com>
2015-04-04 06:54:36 -0700

    :new: Add Basic Worker.js

---
+++ Source/Worker.js
@@ -0,0 +1,40 @@
+
+
+"use strict";
+var
+  CPP = new (require('childprocess-promise')),
+  RedisProto = require('redis-proto'),
+  SupportedEvents = null;
+
+class Worker{
+  constructor(Socket){
+    this.Socket = Socket;
+    this.Levels = 0;
+    Socket.on('data', Worker.OnData.bind(this));
+  }
+  static OnData(Buffer){
+    Buffer = Buffer.toString();
+    let Result = RedisProto.Decode(Buffer);
+    if(!Result.length || SupportedEvents.indexOf(Result[0]) === -1){
+      this.Socket.write("-ERR unknown command '${Result[0] || ''}'\r\n");
+    } else {
+      CPP.Request(Result[0], Result).then(function(Response){
+        if(Response === '+OK'){
+          this.Socket.write(Response + "\r\n");
+        } else {
+          this.Socket.write(RedisProto.Encode(Response) + "\r\n");
+        }
+      }.bind(this));
+    }
+  }
+}
+
+process.on('message', function(C, Instance){
+  if(C !== 'Server') return ;
+  Instance.on('connection', function (Socket){
+    new Worker(Socket);
+  });
+});
+CPP.once('SupportedEvents', function(Events){
+  SupportedEvents = Events;
+});
