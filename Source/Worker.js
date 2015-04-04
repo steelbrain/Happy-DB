de64981a0f37b8878e4b73d3a21987985502baa9
steelbrain <aneesiqbalbhatti@gmail.com>
2015-04-04 07:20:42 -0700

    :art: Remove command name from Request in Worker

--- Source/Worker.js
+++ Source/Worker.js
@@ -18,7 +18,7 @@ class Worker{
     if(!Result.length || SupportedEvents.indexOf(Result[0]) === -1){
       this.Socket.write("-ERR unknown command '${Result[0] || ''}'\r\n");
     } else {
-      CPP.Request(Result[0], Result).then(function(Response){
+      CPP.Request(Result.shift(), Result).then(function(Response){
         if(Response === '+OK'){
           this.Socket.write(Response + "\r\n");
         } else {
