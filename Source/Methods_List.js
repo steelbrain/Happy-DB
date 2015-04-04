583ae0612cd74b88ea70c0b3e0512e26d6c6979a
steelbrain <aneesiqbalbhatti@gmail.com>
2015-04-04 13:21:24 -0700

    :new: Implement LPUSH

--- Source/Methods_List.js
+++ Source/Methods_List.js
@@ -2,3 +2,19 @@
 
 "use strict";
 var Main = module.parent.exports;
+
+Main.ActionLPUSH = function(Request){
+  if(Request.length < 2){
+    throw new Error(Main.MSG_ARGS_LESS);
+  }
+  let Key = Request.shift();
+  let Value = this.Database.get(Key);
+  try {
+    Main.Validate(Main.VAL_LIST, 'LPUSH', Value);
+  } catch(err){
+    Value = [];
+    this.Database.set(Key, Value);
+  }
+  Request.forEach((Item) => Value.push(Item));
+  return {Type: 'OK'};
+};
