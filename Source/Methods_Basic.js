783e31d946b0ea5555ceddd500c93f8ebad6cac6
steelbrain <aneesiqbalbhatti@gmail.com>
2015-04-04 10:47:21 -0700

    :art: Simplify Main::Action{SET, GET, INC}

--- Source/Methods_Basic.js
+++ Source/Methods_Basic.js
@@ -4,53 +4,34 @@
 var Main = module.parent.exports;
 
 Main.ActionSet = function(Child, Request, Message){
-  if(Request.length % 2 !== 0)
-    throw new Error("Invalid Parameter length, Parameter number should be even");
+  Main.ValidateArguments(Main.ARGS_EVEN, Request.length);
   for(let Number = 0; Number < Request.length; Number += 2){
-    let Value = Request[Number + 1];
-    let ValueInt = parseInt(Value);
-    if(!isNaN(ValueInt) && ValueInt.toString() === Value){
-      this.Database.set(Request[Number], ValueInt);
-    } else {
-      this.Database.set(Request[Number], Value);
-    }
+    let Value = Main.NormalizeType(Request[Number + 1]);
+    this.Database.set(Request[Number], Value);
   }
-  Message.Result = {Type: 'OK'};
-  Child.Finished(Message);
+  return {Type: 'OK'};
 };
 
 Main.ActionGET = function(Child, Request, Message){
   if(Request.length === 1){
-    Message.Result = this.Database.get(Request[0]) || null;
-  } else {
-    let ToReturn = [];
-    Request.forEach(function(Name){
-      ToReturn.push(this.Database.get(Name) || null);
-    }.bind(this));
-    Message.Result = ToReturn;
+    return this.Database.get(Request[0]) || null;
   }
-  Child.Finished(Message);
+  return Request.map(function(Name){
+    return this.Database.get(Name) || null;
+  }.bind(this));
 };
 
 Main.ActionINCR = function(Child, Request, Message){
   if(Request.length === 1){
     let Value = this.Database.get(Request[0]) || 0;
-    if(typeof Value !== 'number'){
-      throw new Error("Can't INCR Non-Numeric key ${Request[0]}");
-    }
+    Main.Validate(Main.VAL_NUMERIC, 'INCR', Value);
     this.Database.set(Request[0], ++Value);
-    Message.Result = Value;
-  } else {
-    let ToReturn = [];
-    Request.forEach(function(Name){
-      let Value = this.Database.get(Name) || 0;
-      if(typeof Value !== 'number'){
-        throw new Error("Can't INCR Non-Numeric key ${Name}");
-      }
-      this.Database.set(Request[0], ++Value);
-      ToReturn.push(Value);
-    }.bind(this));
-    Message.Result = ToReturn;
+    return Value;
   }
-  Child.Finished(Message);
+  return Request.map(function(Name){
+    let Value = this.Database.get(Name) || 0;
+    Main.Validate(Main.VAL_NUMERIC, 'INCR', Value);
+    this.Database.set(Request[0], ++Value);
+    return Value;
+  }.bind(this));
 };
