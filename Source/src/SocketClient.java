13275b08d96bdc4d207ac5bcaaacabd6fbc5e6b7
steelbrain <aneesiqbalbhatti@gmail.com>
2015-04-07 13:18:27 -0700

    :new: Use the new RedisProto in SocketClient

--- Source/src/SocketClient.java
+++ Source/src/SocketClient.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.HashMap;
@@ -13,13 +14,18 @@ public class SocketClient implements Runnable {
   }
 
   public void run() {
-
     try {
       BufferedReader InBound = new BufferedReader(new InputStreamReader(ClientSocket.getInputStream()));
-      String Line;
-      String[] LineChunks;
-      while ((Line = InBound.readLine()) != null) {
-        LineChunks = Line.split("\\ ");
+      DataOutputStream OutBound = new DataOutputStream(ClientSocket.getOutputStream());
+      while (ClientSocket.isConnected()) {
+        try {
+          String OutBuffer = RedisProto.Encode(String.join(" ", Happy.Handle(RedisProto.Decode(InBound))));
+          OutBound.writeBytes(OutBuffer);
+        } catch(HappyNullException e){
+          OutBound.writeBytes("$-1\r\n");
+        } catch(HappyErrorException e){
+          OutBound.writeBytes("-" + e.Message + "\r\n");
+        }
       }
       ClientSocket.close();
     } catch(Exception e){
